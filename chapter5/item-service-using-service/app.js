1d57787c2e60ea20b1c7700c54a53eb2a43f20ef
Justin McMinn <akajut@gmail.com>
2015-10-06 17:13:28 -0500

    Pushed before final save

--- chapter5/item-service-using-service/app.js
+++ chapter5/item-service-using-service/app.js
@@ -1,37 +1,37 @@
-// File: chapter5/simple-angularjs-service/app.js
-angular.module('notesApp', [])
-  .controller('MainCtrl', [function() {
+// File: chapter5/item-service-using-service/app.js
+
+function ItemService() {
+  var items = [
+    {id: 1, label: 'Item 0'},
+    {id: 2, label: 'Item 1'}
+  ];
+  this.list = function() {
+    return items;
+  };
+  this.add = function(item) {
+    items.push(item);
+  };
+}
+
+angular.module('notesApp',[])
+  .service('ItemService', [ItemService])
+  .controller('MainCtrl', [function(){
     var self = this;
     self.tab = 'first';
     self.open = function(tab) {
       self.tab = tab;
     };
-}])
-  .controller('SubCtrl', ['ItemService',
-      function(ItemService) {
+  }])
+  .controller('SubCtrl',
+     ['ItemService', function(ItemService) {
     var self = this;
     self.list = function() {
       return ItemService.list();
     };
-
     self.add = function() {
       ItemService.add({
         id: self.list().length + 1,
-        label: 'Item ' + self.list().length
+        label: "Item " + self.list().length
       });
-    };
-  }])
-  .factory('ItemService', [function() {
-    var items = [
-    {id: 1, label: 'Item 0'},
-    {id: 2, label: 'Item 1'}
-    ];
-    return {
-      list: function() {
-        return items;
-      },
-      add: function(item) {
-        items.push(item);
-      }
     };
   }]);
